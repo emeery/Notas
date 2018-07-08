60521bf64ecbcd4036008eed18070cff313b02f6
emeery <gerardoemery@live.com.mx>
2018-07-07 19:26:48 -0500

    eliminar item

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.agregarNota = this.agregarNota.bind(this);
+    this.removerNota = this.removerNota.bind(this);
     this.app = firebase.initializeApp(DB_CONFIG);
     this.basedatos = this.app.database().ref().child('notas');
     // this.state = {
@@ -25,49 +26,69 @@ class App extends Component {
   }
   componentWillMount() {
     const notasPrevias = this.state.notas;
+    // agrega un nuevo item al arreglo con Id y el contenido
+    // y establece el arreglo al nuevo conjunto
     this.basedatos.on('child_added', snap => {
       notasPrevias.push({
         id: snap.key,
         notaContenido: snap.val().notaContenido
       })
-
       this.setState({
         notas: notasPrevias
       })
     })
-
+    // elimina un item del arreglo con splice 
+    this.basedatos.on('child_removed', snap => {
+      for(var i = 0; i < notasPrevias.length; i++){
+        if(notasPrevias[i].id === snap.key) {
+          notasPrevias.splice(i, 1);
+        }
+      }
+      this.setState({
+        notas: notasPrevias
+      })
+    })
     
   }
   agregarNota = (nota) => {
     // introduce la nueva nota dentro del arreglo
     this.basedatos.push().set({ notaContenido: nota });
   }
+  removerNota = (notaId) => {
+    // remueve nota dentro del arreglo
+    console.log('desde el nodo:' + notaId);
+    this.basedatos.child(notaId).remove();
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React y Firebase App</h1>
         </header>
         <div className="App-intro">
-          <h2>Lista React + Firebase</h2>
-          <div>
-          {
-            this.state.notas.map(nota => {
-              return (
-                <Nota
-                key={nota.id}
-                notaContenido={nota.notaContenido}
-                notaId={nota.id}
+            
+              
+                <div >
+                {
+                  this.state.notas.map(nota => {
+                    return (
+                      <Nota
+                      key={nota.id}
+                      notaContenido={nota.notaContenido}
+                      removerNota={this.removerNota}
+                      notaId={nota.id}
+                      />
+                    )
+                  })
+                }
+                </div>
+                <Forma
+                agregarNota={this.agregarNota}
                 />
-              )
-            })
-          }
-          </div>
-          <Forma
-          agregarNota={this.agregarNota}
-          />
+              
         </div>
+        
       </div>
     );
   }
--- src/componentes/Nota.js
+++ src/componentes/Nota.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../Nota.css'
 class Nota extends React.Component {
     constructor(props) {
         super(props);
         this.notaContenido = props.notaContenido;
         this.notaId = props.notaId;
+        this.controlaRemoverNota = this.controlaRemoverNota.bind(this);
+    }
+    controlaRemoverNota(id){
+        this.props.removerNota(id);
     }
     render() { 
         return (
-            <div key={this.notaId}>
+            <div 
+            className="nota_contenido"
+            key={this.notaId}>
                 <h2>{this.notaContenido}</h2>
+                <button 
+                onClick={() => 
+                    this.controlaRemoverNota(this.notaId)
+                }
+                className="x">X</button>
             </div>)
     }
 }
@@ -17,3 +29,5 @@ Nota.propTypes = {
     notaContenido: PropTypes.string.isRequired
 }
 export default Nota;
+
+// onClick={this.controlaRemoverNota(this.notaId)}
